856faa8bcd0d67027634a31a8cd91afa84ca7d9e
Sundaram Sethu <sundaramsethu@gmail.com>
2019-07-24 21:49:45 +0530

    changed to Hi there

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			Console.Write("Hello world. New changed text ");
+			Console.Write("Hi there");
 			Console.ReadKey();
 		}
 	}
